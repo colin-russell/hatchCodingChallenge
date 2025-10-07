7a44d42eb9f141d1df70379ceb8453aa1cca62d1
Colin Russell <colinrussell101@gmail.com>
2025-10-07 13:01:15 -0400

    Initial Commit

---
+++ HatchCodingChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HatchCodingChallenge
+//
+//  Created by Colin Russell on 2025-10-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HatchCodingChallenge/HatchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchCodingChallengeApp.swift
+//  HatchCodingChallenge
+//
+//  Created by Colin Russell on 2025-10-07.
+//
+
+import SwiftUI
+
+@main
+struct HatchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
