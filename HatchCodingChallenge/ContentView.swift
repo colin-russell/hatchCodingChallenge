7b611027d24dbb47a441eb03d2b60a35425ce05b
Colin Russell <colinrussell101@gmail.com>
2025-10-07 20:23:05 -0400

    Fix the remaining warnings

--- HatchCodingChallenge/ContentView.swift
+++ HatchCodingChallenge/ContentView.swift
@@ -54,9 +54,11 @@ struct ContentView: View {
                 .coordinateSpace(name: "scroll")
                 // Capture the ScrollView height from the outer GeometryReader
                 .onAppear { scrollHeight = outerProxy.size.height }
-                .onChange(of: outerProxy.size) { newSize in scrollHeight = newSize.height }
+                .onChange(of: outerProxy.size) { oldSize, newSize in
+                    scrollHeight = newSize.height
+                }
                 // If videos just loaded and nothing is playing yet, start the first video immediately
-                .onChange(of: viewModel.videos.count) { _ in
+                .onChange(of: viewModel.videos.count) { oldCount, newCount in
                     guard viewModel.currentPlayingID == nil, let first = viewModel.videos.first else { return }
                     Task {
                         await viewModel.attemptSetPlayingIfReady(first)
@@ -500,3 +502,4 @@ struct GrowingTextView: View {
     }
 }
 #endif
+
